33fe410339f2ac074bd4dc193694ac95b0be20d4
Vincent McAulay <bwownbwead@hotmail.com>
2016-02-10 08:13:06 +0000

    getting and printin authors

--- src/components/authors/authorsPage.js
+++ src/components/authors/authorsPage.js
@@ -2,12 +2,56 @@
 'use strict';
 
 var React = require('react');
+var AuthorApi = require('../../api/authorApi');
 
 var Authors = React.createClass({
+
+	getInitialState: function () {
+		return {
+			authors: []
+		};
+	},
+
+	componentWillMount: function () {
+		this.setState({
+			authors: AuthorApi.getAllAuthors()
+		});
+	},
+
 	render: function() {
+
+		var createAuthorRow = function (author) {
+			return (
+				<tr key={author.id}>
+					<td>
+						<a href={"/#authors/" + author.id}>
+							{ author.id }
+						</a>
+					</td>
+					<td>
+						{ author.firstName } { author.lastName }
+					</td>
+				</tr> 
+			);
+		};
+
 		return (
 			<div>
-				"Some HTML"
+				<table className="table">
+					<thead>
+						<tr>
+							<th>
+								ID
+							</th>
+							<th>
+								Name
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{ this.state.authors.map(createAuthorRow, this) }
+					</tbody>
+				</table>
 			</div>
 		);
 	}
