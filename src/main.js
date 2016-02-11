bb9d94fd7f6c2ba226a243fb072d34ebd0547367
Vincent McAulay <v.mcaulay@amaze.com>
2016-02-11 08:33:58 +0000

    added IIFE and strict

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
@@ -5,36 +6,42 @@ var About = require('./components/about/aboutPage');
 var Authors = require('./components/authors/authorsPage');
 var Header = require('./common/header');
 
-var App = React.createClass({
-	render: function () {
-		var Child;
+(function () {
 
-		switch(this.props.route) {
-			case 'about':
-				Child = About;
-				break;
-			case 'authors':
-				Child = Authors;
-				break;
+	'use strict';
 
-			default:
-				Child = Home;
+	var App = React.createClass({
+		render: function () {
+			var Child;
+
+			switch(this.props.route) {
+				case 'about':
+					Child = About;
+					break;
+				case 'authors':
+					Child = Authors;
+					break;
+
+				default:
+					Child = Home;
+			}
+
+			return (
+				<div>
+					<Header />
+					<Child />
+				</div>
+			);
 		}
+	});
 
-		return (
-			<div>
-				<Header />
-				<Child />
-			</div>
-		);
+	function render() {
+		var route = window.location.hash.substr(1);
+		React.render(<App route={route} />, document.getElementById('app'));
 	}
-});
 
-function render() {
-	var route = window.location.hash.substr(1);
-	React.render(<App route={route} />, document.getElementById('app'));
-}
+	window.addEventListener('hashchange', render);
+	render();
 
-window.addEventListener('hashchange', render);
-render();
+})();
 
