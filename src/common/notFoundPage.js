d61cd37a0ffe807c61dd6a6c8b67fbf9b841f85a
Vincent McAulay <v.mcaulay@amaze.com>
2016-02-16 13:04:37 +0000

    added 404 page and routing

---
+++ src/common/notFoundPage.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
+var Header = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h1>Page not found</h1>
+			</div>
+		);
+	}
+});
+
+module.exports = Header;
